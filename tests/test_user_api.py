721a95bacea73ac257c8eb9e5afea1254d77daca
harshitpant671 <97526903+harshitpant671@users.noreply.github.com>
2025-05-25 14:20:36 +0530

    Update test_user_api.py

--- tests/test_user_api.py
+++ tests/test_user_api.py
@@ -30,6 +30,8 @@ def test_create_users(api_client, load_user_data):
     print("POST response:", response.json())
     assert response.status_code == 201
     assert response.json()['name'] == user_data['name']
+    response = api_client.get("users")
+    print(response.json())
     
     # Extract ID and fetch user
     id = response.json()['id']    
